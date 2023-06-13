69c478c07287eeb99f4de19c547b4d1aef6c7d86
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-12 16:20:28 -1000

    feat: add local onig.wasm

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "standalone",
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/preview": ["./node_modules/vscode-oniguruma/**/*"],
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/lib/highlight.ts
+++ src/lib/highlight.ts
@@ -1,14 +1,22 @@
 import { all, createStarryNight } from "@wooorm/starry-night";
 import { toHtml } from "hast-util-to-html";
+import fs from "fs";
+import path from "path";
 
 let starryNight: Awaited<ReturnType<typeof createStarryNight>>;
+
+fs.readFile(path.join(process.cwd(), "src/onig.wasm"), () => {});
 
 export async function highlight(
   input: string,
   language: string
 ): Promise<string> {
   if (!starryNight) {
-    starryNight = await createStarryNight(all);
+    starryNight = await createStarryNight(all, {
+      getOnigurumaUrlFs: async () => {
+        return new URL("./src/onig.wasm", `file://${process.cwd()}/`);
+      },
+    });
   }
 
   const scope = starryNight.flagToScope(language);
