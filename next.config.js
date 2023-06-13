4b0c2b91f854fc014ebdb6c523d141caf63cab06
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-12 16:29:24 -1000

    refactor: move onig.wasm to a vendor dir

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/preview": ["./node_modules/vscode-oniguruma/**/*"],
-    },
-  },
 };
 
 module.exports = nextConfig;
--- src/lib/highlight.ts
+++ src/lib/highlight.ts
@@ -1,20 +1,27 @@
 import { all, createStarryNight } from "@wooorm/starry-night";
 import { toHtml } from "hast-util-to-html";
-import fs from "fs";
+import * as fs from "node:fs/promises";
 import path from "path";
 
 let starryNight: Awaited<ReturnType<typeof createStarryNight>>;
-
-fs.readFile(path.join(process.cwd(), "src/onig.wasm"), () => {});
 
 export async function highlight(
   input: string,
   language: string
 ): Promise<string> {
+  // Needed for Next.js to include the vendored `onig.wasm` file.
+  // This code should have no effect in production.
+  if (false) {
+    fs.readFile(path.join(process.cwd(), "vendor/vscode-oniguruma/onig.wasm"));
+  }
+
   if (!starryNight) {
     starryNight = await createStarryNight(all, {
       getOnigurumaUrlFs: async () => {
-        return new URL("./src/onig.wasm", `file://${process.cwd()}/`);
+        return new URL(
+          "./vendor/vscode-oniguruma/onig.wasm",
+          `file://${process.cwd()}/`
+        );
       },
     });
   }
